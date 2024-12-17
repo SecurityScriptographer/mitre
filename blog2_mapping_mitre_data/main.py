ad2851dd1ef21bf9996171e68634717eef469b0a
Robin dos Santos <rd563381@fh-muenster.de>
2024-12-17 15:24:20 +0100

    init commit with blog1 and blog2

---
+++ blog2_mapping_mitre_data/main.py
@@ -0,0 +1,25 @@
+import logging
+import json
+from config import FULL_PATH, setup_logging
+from loader import load_all_data
+from mapper import map_all_data
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    setup_logging()
+    
+    data = load_all_data(use_cache=True)
+
+    logger.info(f"Loaded {len(data['techniques'])} techniques")
+    logger.info(f"Loaded {len(data['relationships'])} relationships")
+    logger.info(f"Loaded {len(data['groups'])} groups")
+    logger.info(f"Loaded {len(data['mitigations'])} mitigations")
+    
+    
+    logger.info(f"Map data")
+    data = map_all_data(data=data)
+    logger.info(f"Mapped all data")
+    
+    with open(FULL_PATH, 'w+') as json_file:
+        json.dump(data, json_file)
